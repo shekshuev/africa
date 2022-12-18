603705d8d34143db0805704cd14f8cb650e3909a
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2022-12-18 23:03:12 +0300

    Lesson 1 done

---
+++ Africa/Extension/CodableBundleExtension.swift
@@ -0,0 +1,24 @@
+//
+//  CodableBundleExtension.swift
+//  Africa
+//
+//  Created by Sergei Shekshuev on 18.12.2022.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode(_ file: String)-> [CoverImage] {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Unable to locate \(file) in bundle")
+        }
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle")
+        }
+        let decoder = JSONDecoder()
+        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle")
+        }
+        return loaded
+    }
+}
---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Sergei Shekshuev on 18.12.2022.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
